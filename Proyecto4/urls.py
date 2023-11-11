c01ac333e3a656f3fe5c22b2cc1c34a043e15453
Santiago3896 <santigini96@gmail.com>
2023-11-11 18:27:57 -0300

    templates-urls

--- Proyecto4/urls.py
+++ Proyecto4/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from Proyecto4.views import inicio
+from Proyecto4.views import Inicio, About, Contact,Frameworks,Languages
 
 urlpatterns = [
-    path("",inicio,name = "inicio"),
+    path("",Inicio,name = "inicio"),
+    path("/About",About,name = "about"),
+    path("/Contact",Contact,name = "contact"),
+    path("/Languages",Languages,name = "languages"),
+    path("/Frameworks",Frameworks,name = "frameworks"),
 ]
--- Proyecto4/views.py
+++ Proyecto4/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def inicio(request):
+def Inicio(request):
     
-    return render(request,"inicio/inicio.html")
+    return render(request,"inicio/Inicio.html")
+
+def About(request):
+    
+    return render(request,"inicio/About.html")
+
+def Contact(request):
+    
+    return render(request,"inicio/Contact.html")
+
+def Contact(request):
+    
+    return render(request,"inicio/Contact.html")
+
+def Languages(request):
+    
+    return render(request,"inicio/Languages.html")
+
+def Frameworks(request):
+    
+    return render(request,"inicio/Frameworks.html")
