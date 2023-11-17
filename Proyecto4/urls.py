d3fba7f7ec1faffdfaa11223611dcd0842f57ecc
Santiago3896 <santigini96@gmail.com>
2023-11-17 18:08:51 -0300

    commit

--- Proyecto4/urls.py
+++ Proyecto4/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from Proyecto4.views import Inicio, About, Contact,Frameworks,Languages
+from Proyecto4.views import Inicio, About, Contact, Creacion_Frameworks, Creacion_Languages, Languages,Frameworks
 
 urlpatterns = [
     path("",Inicio,name = "inicio"),
@@ -8,5 +8,8 @@ urlpatterns = [
     path("/Contact",Contact,name = "contact"),
     path("/Languages",Languages,name = "languages"),
     path("/Frameworks",Frameworks,name = "frameworks"),
+    path("/CreacionFrameworks",Creacion_Frameworks,name = "CreacionFrameworks"),
+    path("/CreacionLanguages",Creacion_Languages,name = "CreacionLanguages"),
+    
     # (EL PATH QUE QUIERO PONERLE (Es indistinto)  -- LA VIEW QUE QUIERO MOSTRAR -- PARA CONECTAR CON EL TEMPLATE BASE (Opcional))
 ]
--- Proyecto4/views.py
+++ Proyecto4/views.py
@@ -6,10 +6,20 @@ from Proyecto4.models import Language, Framework
 # Create your views here.
 
 def Inicio(request):
-    
-    return render(request,"inicio/Inicio.html")
+    listado_languages = Language.objects.all()
+    return render(request,"inicio/Inicio.html",{"listadoL": listado_languages})
 
 def Languages(request):
+    listadoL = Language.objects.all()
+    print(listadoL)
+    return render(request,"inicio/Languages.html",{"listadoL": listadoL})
+
+def Frameworks(request):
+    listado_frameworks = Framework.objects.all()
+    print("pasando")
+    return render(request,"inicio/Frameworks.html",{"listadoF": listado_frameworks})
+
+def Creacion_Languages(request):
     
     if request.method == "POST":
         formularioLanguages = CreacionLanguages(request.POST)
@@ -27,35 +37,36 @@ def Languages(request):
             languages = Language(nombre = nombre , creador = creador , version = version , descripcion = descripcion)
              
             languages.save()
+            print(languages)
             return redirect(Inicio)
         else: 
-            return render(request, "", {"formularioLanguages": formularioLanguages})
-    formulario = CreacionLanguages()
-    return render(request,"inicio/Languages.html", {"formulario": formulario})       
+            print("pasando por aca")
+            return render(request,"", {"formularioLanguages": formularioLanguages})
+    formularioL = CreacionLanguages()
+    return render(request,"inicio/CreacionLanguages.html", {"formularioLl": formularioL})       
     
 
-def Frameworks(request):
+def Creacion_Frameworks(request):
+    
     if request.method == "POST":
         formularioFramework = CreacionFramework(request.POST)
         if formularioFramework.is_valid():
             info_limpiaFramework = formularioFramework.cleaned_data
-            
+            # AGARRO EL DATO DEL FORM Y LO GUARDO EN VARIABLE
             nombre = info_limpiaFramework.get("nombre")
             languages = info_limpiaFramework.get("languages")
             descripcion = info_limpiaFramework.get("descripcion")
-            
+            # DONDE DIGA TAL COSA EN EL MODELO REMPLAZALO POR TAL VARIABLE. SAVE GUARDA EN EL MODELO
             framework = Framework(nombre = nombre , languages = languages , descripcion = descripcion)
             framework.save()
+            # REDIRIGIR A LA VIEW INICIO
             return redirect(Inicio)
         else:
+            # SI NO ES VALIDO EL FORMULARIO, SE MUESTRA EL ERROR PERO NO ME ESTA FUNCIONANDO- ARREGLAR -
             return render(request, "", {"formularioFramework": formularioFramework}) 
     formularioF = CreacionFramework()
-    return render(request,"inicio/Frameworks.html", {"formularioF": formularioF})
-            
-            
-    
-    
-    return render(request,"inicio/Frameworks.html")
+    # LE PASO EL FORMULARIO VACIO ABAJO, PORQUE LA PRIMER VISTA VIENE POR GET
+    return render(request,"inicio/CreacionFrameworks.html", {"formularioFf": formularioF})
 
 
 def About(request):
