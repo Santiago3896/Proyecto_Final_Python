2252ae7929ee48c287b15afa1739c80db914f990
Santiago3896 <santigini96@gmail.com>
2023-11-18 16:47:05 -0300

    complete model con views de class

--- navegadoresWebs/views.py
+++ navegadoresWebs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 
 from navegadoresWebs.models import Navegador
@@ -15,4 +16,18 @@ class CreacionNavegadores(CreateView):
     template_name = "navegadores/creacionNavegadores.html"
     fields = ["nombre", "version", "fecha_creacion"]
     success_url = reverse_lazy("navegadores")
-    
+
+class ActualizarNavegadores(UpdateView):
+    model = Navegador
+    template_name = "navegadores/actualizarNavegadores.html"
+    fields = ["nombre", "version", "fecha_creacion"]
+    success_url = reverse_lazy("navegadores")
+
+class DetalleNavegador(DetailView):
+    model = Navegador
+    template_name = "navegadores/detalleNavegadores.html"
+
+class EliminarNavegador(DeleteView):
+    model = Navegador
+    template_name = "navegadores/eliminarNavegadores.html"
+    success_url = reverse_lazy("navegadores")
