be91689510bc70076fa83b040ac0c58260c94ac0
Santiago3896 <santigini96@gmail.com>
2023-11-18 17:22:32 -0300

    ok

--- Proyecto4/admin.py
+++ Proyecto4/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from Proyecto4.models import Language,Framework
+
+admin.site.register([Language, Framework])
+# admin.site.register(Framework)
